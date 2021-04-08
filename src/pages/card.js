a6d37bf9241355ee909e39b2f7e202d8eff4817f
Yash Masani <yash.masani@gmail.com>
2021-04-07 22:12:42 -0400

    Added new home screen

---
+++ src/pages/card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default function card(){
+  return (
+
+   <div classame="card">
+      <div className="image">
+        <img alt="Blog"/>
+        <p className="date"></p>
+      </div>
+      <h1></h1>
+      <p></p>
+      <a href="#top"></a>
+   </div>
+    
+  )
+}
