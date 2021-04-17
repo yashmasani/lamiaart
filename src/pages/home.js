c138bf5b620da5a3802a5a71380e9785179ea709
Yash Masani <yash.masani@gmail.com>
2021-04-17 16:32:26 -0400

    Change Background image and partially added navbar

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import {Link} from "gatsby"
+import "../styles/nav.scss"
+import {StaticImage} from "gatsby-plugin-image"
 
 export default function home(){
   return (
-    <p>This is the future navbar</p>
+    <NavBar></NavBar>
   ) 
+}
+
+const NavBar = props => {
+
+  
+  if(window.innerWidth > 500){
+
+   return (
+      <nav>
+        <Link to="/Articles">Articles</Link>
+        <Link to="/About">About</Link>
+        <StaticImage src="../images/smallerLogo.png" alt="Logo"></StaticImage>
+        <Link to="/Pages">Pages</Link>
+        <Link to="/Poetry">Poetry</Link>
+      </nav>
+   )
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import * as React from "react"
 import {StaticImage} from "gatsby-plugin-image"
 import "../styles/index.scss"
-import "@fontsource/italianno"
 import {Link} from "gatsby"
-
+import "@fontsource/playfair-display-sc";
 // markup
 export default function IndexPage(){
   return(
     <div className="index">
-    <StaticImage src="../images/ladyLongHair.jpg" alt="long hair Lady" placeholder="blurred" className="lady"></StaticImage>
-    <Link to="/home"><StaticImage src="../images/logo.png" alt="lamiaarti" placeholder="blurred" className="logo" style={{position:"absolute"}}></StaticImage></Link>
     <p className="font-index">Unfold My Story...</p>
+    <StaticImage src="../images/new.jpeg" alt="long hair Lady" placeholder="blurred" className="lady"></StaticImage>
+    <Link to="/home"><StaticImage src="../images/logo.png" alt="lamiaarti" placeholder="blurred" className="logo" style={{position:"absolute"}}></StaticImage></Link>
    </div>
   )
 }
